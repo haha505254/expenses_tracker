23c43429d4e944dc423d5cd2756efdddc3b09c1d
haha505254 <haha505254@gmail.com>
2022-07-09 20:09:23 +0800

    add funtion created

--- expenses_tracker/expenses_log/urls.py
+++ expenses_tracker/expenses_log/urls.py
@@ -2,5 +2,6 @@
 from django.urls import path
 from expenses_log import views
 urlpatterns = [
-    path('', views.listall),
+    path('', views.listall,name='listall'),
+    path('insert/', views.insert,name='insert'),
 ]
--- expenses_tracker/expenses_log/views.py
+++ expenses_tracker/expenses_log/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
 from expenses_log.models import Expense
 
 # Create your views here.
@@ -9,3 +10,15 @@ def listall(request):
     expenses = Expense.objects.all().order_by('id')  
     #讀取資料表, 依 id 遞增排序(欄位前加入負號-id代表遞減排序)
     return render(request, "expenses_log/index.html", locals())
+
+def insert(request):  #新增資料
+    try:
+        if request.method == 'POST':
+            name = request.POST.get('name')
+            price = request.POST.get('price')
+            unit = Expense.objects.create(name=name, price=price) 
+            unit.save()  #寫入資料庫
+    except Exception as e:
+        return HttpResponse("請填入正確數字")
+    expenses = Expense.objects.all().order_by('-id')  #讀取資料表, 依 id 遞減排序
+    return redirect('listall')
